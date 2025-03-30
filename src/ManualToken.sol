160ac9dae68b80d79be903da2cabae015bc633c3
abyssvgc <abdeliniaramayo@gmail.com>
2025-03-30 13:44:00 -0400

    fix: update Solidity version to 0.8.29 and change function visibility in ManualToken

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,30 +1,33 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.29;
+
+event Transfer(address indexed from, address indexed to, uint256 value);
 
 contract ManualToken {
     mapping(address => uint256) private s_balances;
 
-    function name() public pure returns (string memory) {
+    function name() external pure returns (string memory) {
         return "Manual Token";
     }
 
-    function totalSupply() public pure returns (uint256) {
+    function totalSupply() external pure returns (uint256) {
         return 100 ether;
     }
 
-    function decimals() public pure returns (uint8) {
+    function decimals() external pure returns (uint8) {
         return 18;
     }
 
-    function balanceOf(address _owner) public view returns (uint256) {
+    function balanceOf(address _owner) external view returns (uint256) {
         return s_balances[_owner];
     }
 
-    function transfer(address _to, uint256 _amount) public returns (bool) {
+    function transfer(address _to, uint256 _amount) external returns (bool) {
         require(s_balances[msg.sender] >= _amount, "Insufficient balance");
         s_balances[msg.sender] -= _amount;
         s_balances[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
         return true;
     }
 }
